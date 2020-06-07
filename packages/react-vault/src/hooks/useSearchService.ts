9d5ab2416aa04ecdc4ea420e58e831513127b8f7
Stephen Fraser <stephen.fraser@digirati.com>
2020-06-07 12:38:29 +0100

    Updated react-vault to final Presentation 3

--- packages/react-vault/src/hooks/useSearchService.ts
+++ packages/react-vault/src/hooks/useSearchService.ts
@@ -2,11 +2,14 @@
 // Final API to be decided, but this hook will be for driving search querying itself
 // Another hook/context will handle actually getting the search results for a particular canvas
 import { useManifest } from './useManifest';
-import { ServiceNormalized } from '@hyperion-framework/types';
+import { SearchService, ServiceNormalized } from '@hyperion-framework/types';
 
-export function useSearchService(): ServiceNormalized | undefined {
+export function useSearchService(): SearchService | undefined {
   const manifest = useManifest();
   return manifest
-    ? manifest.service.find((service: ServiceNormalized) => service.profile === 'SearchService1')
+    ? (manifest.service.find(
+        (service: ServiceNormalized) =>
+          service.profile === 'SearchService1' || service.profile === 'http://iiif.io/api/search/1/search'
+      ) as any)
     : undefined;
 }
