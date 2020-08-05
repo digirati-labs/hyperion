6e103596f3a01d4afd0630d3372eb07d47aa34e1
stephenwf <stephen.fraser@digirati.com>
2020-08-05 14:24:20 +0100

    Modify build

--- packages/react-vault/rollup.config.js
+++ packages/react-vault/rollup.config.js
@@ -4,6 +4,9 @@ import pkg from './package.json';
 export default createRollupConfig('HyperionReactVault', pkg, [
   'react',
   'react-dom',
+  'redux',
+  'react-query',
+  '@atlas-viewer/iiif-image-api',
   '@hyperion-framework/vault',
   '@hyperion-framework/store',
   '@hyperion-framework/parser',
