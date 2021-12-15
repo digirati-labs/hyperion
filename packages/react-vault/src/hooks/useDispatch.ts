39dd4fa3ceea1f3ddbcc3066b1b74d8564653ae4
Stephen Fraser <stephen.fraser@digirati.com>
2021-12-15 12:46:54 +0000

    Update useDispatch.ts

--- packages/react-vault/src/hooks/useDispatch.ts
+++ packages/react-vault/src/hooks/useDispatch.ts
@@ -6,10 +6,7 @@ export function useDispatch(): ReduxStore['dispatch'] {
   const vault = useVault();
   const store = vault.getStore();
 
-  return useMemo(
-    () => {
-      return (action: any) => store.dispatch(action);
-    },
-    [store]
-  );
+  return useMemo(() => {
+    return (action: any) => store.dispatch(action);
+  }, [store]);
 }
